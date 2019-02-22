09e01b38a4be101de73e8a673dc4d93c71aef4b0
Gunhan Sancar <gunhan.sancar@zoyoglobal.com>
2019-02-22 15:40:09 +0000

    Update

--- app/src/main/java/com/zeugmasolutions/localeexample/Application.kt
+++ app/src/main/java/com/zeugmasolutions/localeexample/Application.kt
@@ -1,14 +1,6 @@
 package com.zeugmasolutions.localeexample
 
 import com.zeugmasolutions.localehelper.LocaleAwareApplication
-import java.util.*
 
 class Application : LocaleAwareApplication() {
-    override fun onCreate() {
-        super.onCreate()
-
-
-        val locales = Locale.getAvailableLocales()
-        val a = locales
-    }
 }
