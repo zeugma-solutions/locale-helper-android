a89bd8cf0bcad87858a6e16cf231f799213f144a
ABQ App Source <abqappsource@gmail.com>
2021-05-16 07:20:55 -0600

    Update example app to use a radio group language selector

--- app/src/main/java/com/zeugmasolutions/localeexample/SecondActivity.kt
+++ app/src/main/java/com/zeugmasolutions/localeexample/SecondActivity.kt
@@ -1,6 +1,7 @@
 package com.zeugmasolutions.localeexample
 
 import android.os.Bundle
+import com.zeugmasolutions.localehelper.LocaleHelper
 import com.zeugmasolutions.localehelper.Locales
 import kotlinx.android.synthetic.main.activity_second.*
 import kotlinx.android.synthetic.main.view_language_buttons.*
@@ -14,13 +15,40 @@ class SecondActivity : BaseActivity() {
 
         setTitle(R.string.second_activity_title)
 
-        toTRButton.setOnClickListener { updateLocale(Locales.Turkish) }
-        toENButton.setOnClickListener { updateLocale(Locale.ENGLISH) }
-        toCNButton.setOnClickListener { updateLocale(Locale.CHINA) }
-        toURButton.setOnClickListener { updateLocale(Locales.Urdu) }
+        language_picker.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId) {
+                R.id.language_default -> updateLocale(null)
+                R.id.language_tr -> updateLocale(Locales.Turkish)
+                R.id.language_en -> updateLocale(Locales.EnglishUS)
+                R.id.language_cn -> updateLocale(Locale.CHINA)
+                R.id.language_ur -> updateLocale(Locales.Urdu)
+            }
+        }
+
+        setLanguageSelection()
 
         backButton.setOnClickListener { finish() }
 
         mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setLanguageSelection()
+    }
+    
+    private fun setLanguageSelection() {
+        var languageSelection = R.id.language_default
+        if( LocaleHelper.hasLocaleSelection(this) ) {
+            languageSelection = when(LocaleHelper.getLocale(this)) {
+                Locales.Turkish -> R.id.language_tr
+                Locales.EnglishUS -> R.id.language_en
+                Locale.CHINA -> R.id.language_cn
+                Locales.Urdu -> R.id.language_ur
+                else -> R.id.language_default
+            }
+        }
+
+        language_picker.check(languageSelection)
     }
 }
